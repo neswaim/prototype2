fb38f659d56d9d2c0a4cfda1105bfc1109cd1032
neswaim <77861348+neswaim@users.noreply.github.com>
2022-09-05 13:03:35 -0400

    Add files via upload

---
+++ Proto1/Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    public AudioSource audioSource;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.constraints = RigidbodyConstraints2D.None;
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D c2d)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.0f);
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.Play();
+    }
+}
